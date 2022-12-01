bb8aea522e81401d23f4a59e71b87448922a7591
Donatien KAMARY <donatkamary@gmail.com>
2022-12-01 14:37:48 +0400

    Update datetime

--- resources/views/_partials/front/forms/search-ride-hero-section.blade.php
+++ resources/views/_partials/front/forms/search-ride-hero-section.blade.php
@@ -1,5 +1,9 @@
 {{-- Search ride hero section --}}
 
+@php
+    $now = new \DateTime();
+@endphp
+
 <form action="{{ route('ride_search') }}" method="GET" class="form row g-0 form-search-ride-hero">
     <div class="form-group  col-xl-3 col-lg-3 col-md-12 mb-0 bg-white ">
         <input type="text" class="form-control input-lg br-te-md-0 br-be-md-0 border-end-0" name="origin" id="search_origin" placeholder="Départ">
@@ -14,12 +18,12 @@
         <input type="hidden" name="destination_lng" id="search_destination_lng">
     </div>
     <div class="form-group col-xl-3 col-lg-3 col-md-12 select2-lg  mb-0 bg-white">
-        <input type="datetime" class="form-control input-lg br-md-0 border-end-0" name="search_date" id="search_date" placeholder="Aujourd'hui">
+        <input type="date" class="form-control input-lg br-md-0 border-end-0" name="search_date" id="search_date" placeholder="Aujourd'hui" min="{{ $now->format('d/m/Y') }}">
         <span><i class="fa fa-calendar location-gps me-1"></i></span>
     </div>
     <div class="form-group col-xl-1 col-lg-1 col-md-12 select2-lg  mb-0 bg-white">
-        <span><i class="fa fa-user location-gps me-1"></i></span>
-        <input type="text" class="form-control input-lg br-md-0 border-end-0" name="search_count" id="search_count" placeholder="">
+        {{-- <span><i class="fa fa-user location-gps me-1"></i></span> --}}
+        <input type="number" class="form-control input-lg br-md-0 border-end-0" name="search_count" id="search_count" placeholder="1" value="1" min="1" max="7">
     </div>
     <div class="col-xl-2 col-lg-2 col-md-12 mb-0">
         <button type="submit" class="btn btn-lg btn-block btn-primary br-ts-md-0 br-bs-md-0">
