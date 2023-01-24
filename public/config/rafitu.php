f2b243a191e3bd1c8bb8697f69447aff14967eaf
Donatien KAMARY <donatkamary@gmail.com>
2023-01-24 23:39:32 +0400

    Deleted unused files

--- public/config/rafitu.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'admin' => env('RAFITU_AMDIN', 'donatkamary@gmail.com'),
-    'cci_mail' => env('RAFITU_CCI_MAIL', true),
-    'cci_email' => env('RAFITU_CCI_EMAIL', 'donatkamary@gmail.com'),
-];
