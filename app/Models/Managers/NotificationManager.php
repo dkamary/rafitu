40df67d220fdf042f6f2e50347012ed593ec040d
Donatien KAMARY <donatkamary@gmail.com>
2023-02-16 14:52:33 +0400

    Updates

--- app/Models/Managers/NotificationManager.php
+++ app/Models/Managers/NotificationManager.php
@@ -17,7 +17,7 @@ class NotificationManager {
         try {
             $headers = "MIME-Version: 1.0 \r\n";
             $headers .= "Content-type:text/html;charset=UTF-8 \r\n";
-            $headers .= "From: RAFITU <noreply@rafitu.com> \r\n";
+            $headers .= 'From: '. config('rafitu.sender') ." \r\n";
             $headers .= 'X-Mailer: PHP/' . phpversion() ."\r\n";
 
             mail($to, $subject, $content, $headers);
@@ -87,43 +87,5 @@ class NotificationManager {
     public static function createNotificationError(?int $userId, string $title, ?string $content = null, ?string $link = null) : ?Notification {
         return self::createNotification(Notification::TYPE_ERROR, $userId, $title, $content, $link);
     }
-
-    // public static function adminNewRide(Ride $ride) {
-    //     $subject = 'Nouveau trajet ajouté';
-    //     $content = view('templates.emails.admin.ride-new', [
-    //         'ride' => $ride,
-    //     ])->render();
-
-    //     self::sendEmail(config('rafitu.admin'), $subject, $content);
-    // }
-
-    // public static function adminNewReservation(Reservation $reservation) {
-    //     $subject = 'Nouvelle réservation';
-    //     $content = view('templates.emails.admin.reservation-new', [
-    //         'reservation' => $reservation,
-    //     ])->render();
-
-    //     self::sendEmail(config('rafitu.admin'), $subject, $content);
-    // }
-
-    // public static function adminNewPaiement(Order $order) {
-    //     //
-    // }
-
-    // public static function ownerNewRide(Ride $ride) {
-    //     //
-    // }
-
-    // public static function ownerNewReservation(Reservation $reservation) {
-    //     //
-    // }
-
-    // public static function ownerNewPaiement(Order $order) {
-    //     //
-    // }
-
-    // public static function adminCancelReservation(Reservation $reservation) {
-    //     //
-    // }
 
 }
--- config/rafitu.php
+++ config/rafitu.php
@@ -4,4 +4,5 @@ return [
     'admin' => env('RAFITU_AMDIN', 'donatkamary@gmail.com'),
     'cci_mail' => env('RAFITU_CCI_MAIL', true),
     'cci_email' => env('RAFITU_CCI_EMAIL', 'donatkamary@gmail.com'),
+    'sender' => env('RAFITU_SENDER', 'Rafitu <rafitu@rafitu.com>'),
 ];
